9371fb64b51f6f874993b9dcd651b6449c1599bb
notInWonderland <75802115+podvalniypiero@users.noreply.github.com>
2023-02-07 20:07:31 +0300

    slides in js

--- script.js
+++ script.js
@@ -31,4 +31,22 @@ const prev = document.querySelector(".previousBtn");
 console.log(next);
 console.log(prev);
 
-const slides = ["slides\blur.jpg","slides\bar.jpg","slides\beautiful-young-woman-sitting-restaurant-talking-mobile-phone.jpg","slides\bowls.jpg","slides\client.jpg","slides\coffee-make.jpg","slides\coffee.jpg","slides\filter.jpg","slides\green.jpg","slides\greens.jpg","slides\hot-matcha.jpg","slides\interior.jpg","slides\lights.jpg","slides\matcha.jpg","slides\pie.jpg","slides\salad.jpg","slides\soup.jpg","slides\talking.jpg","slides\teapot.jpg","slides\vegetables.jpg","slides\water.jpg"];
+const slides = ["slides\\blur.jpg","slides\\bar.jpg","slides\\beautiful-young-woman-sitting-restaurant-talking-mobile-phone.jpg","slides\\bowls.jpg","slides\\client.jpg","slides\\coffee-make.jpg","slides\\coffee.jpg","slides\\filter.jpg","slides\\green.jpg","slides\\greens.jpg","slides\\hot-matcha.jpg","slides\\interior.jpg","slides\\lights.jpg","slides\\matcha.jpg","slides\\pie.jpg","slides\\salad.jpg","slides\\soup.jpg","slides\\talking.jpg","slides\\teapot.jpg","slides\\vegetables.jpg","slides\\water.jpg"];
+console.log(slides[0]);
+let i=0;
+
+next.addEventListener("click",()=>{
+    i++;
+    if (i > slides.length - 1){
+        i = 0;
+    }
+    document.querySelector(".slidesArray").src = slides[i];
+})
+
+prev.addEventListener("click", ()=> {
+    i--;
+    if (i < 0){
+        i = slides.length - 1;
+    }
+    document.querySelector(".slidesArray").src = slides[i];
+})
