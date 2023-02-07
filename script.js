fdec1ad0d17d44bda65a6990b23488b0cf832c81
notInWonderland <75802115+podvalniypiero@users.noreply.github.com>
2023-02-07 19:02:48 +0300

    js for btn

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-gsap .from( ".photo-img",{duration: 5, delay: 4, y: 100, opacity: 0}) 
+//gsap .from( ".photo-img",{duration: 5, delay: 4, y: 100, opacity: 0}) 
+
+const musicBtn = document.querySelector("#musicBtn");
+console.log(musicBtn);
+
+musicBtn.addEventListener("click",function(){
+    document.querySelector("#musicForBtn").play();
+    musicBtn.textContent="Click here to stop"
+
+})
